94c33fbec1a9a5ab88f0c10944e5eaa8802d97e6
dreddy <devendren.reddy@savogroup.com>
2016-09-27 08:56:38 -0500

    order report correctly

--- university-api/src/main/java/com/reddy/university/api/models/Report.java
+++ university-api/src/main/java/com/reddy/university/api/models/Report.java
@@ -12,11 +12,14 @@ public class Report {
     @JsonProperty(value = "Class Sections Taught")
     public MultiValueMap universityClassReport;
 
-    @JsonProperty(value = "How many students are registered for each class")
-    public Map<String, Integer>  universityClassBreakdown;
-
     @JsonProperty(value = "Classes Taken by Each Student")
     public Map<Integer,List<String>> studentClassReport;
+
+    @JsonProperty(value = "Professors with at least 2 classes with 2 or more of the same students")
+    public Map<String, List<String>> professorWith2OrMoreOfSameStudent;
+
+    @JsonProperty(value = "How many students are registered for each class")
+    public Map<String, Integer>  universityClassBreakdown;
 
     @JsonProperty(value = "How many students take more than one class")
     public Map<Integer,List<Integer>> studentClassCountReport;
@@ -24,6 +27,4 @@ public class Report {
     @JsonProperty(value = "How many professors teach more than one class")
     public Map<Integer,List<String>> professorClassCountReport;
 
-    @JsonProperty(value = "Professors with at least 2 classes with 2 or more of the same students")
-    public Map<String, List<String>> professorWith2OrMoreOfSameStudent;
 }
